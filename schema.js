5f33d5510012d3eded0188b9d15e627fa8cb7f84
Alexandru317Mitrica <35486525+Alexandru317Mitrica@users.noreply.github.com>
2018-01-16 12:39:26 +0200

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,1 +1,43 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLNonNull,
+} from 'graphql';
+
+import axios from 'axios';
+
+let AngleInfo = new GraphQLObjectType({
+  name: "AngleInfo",
+  fields: () => ({
+    "login": { type: GraphQLString },
+    "url": { type: GraphQLString },
+  })
+});
+
+const query = new GraphQLObjectType({
+  name: "Query",
+  fields: () => ({
+    testing_query : {
+      type: AngleInfo,
+      args: {
+        login : {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (_,{login}) => {
+        const url = `https://api.github.com/orgs/${login}`;
+        return axios.get(url)
+                    .then(function(response) {
+                      return response.data;
+                    });
+      }
+    },
+  })
+});
+
+const schema = new GraphQLSchema({
+  query
+});
 
